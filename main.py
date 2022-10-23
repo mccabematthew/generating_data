73e9a6e78f2d31754a54a5e8ec5b7105d37d570d
Matthew McCabe <matthew.james.mccabe@outlook.com>
2022-10-22 23:34:58 -0500

    Added input vals list to so plot func will not have to assume how the input numbers were generated

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # plt as alias for pyplot for ease of use
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 # fig represents  'figure' (entire collection of plots), ax reps singular plots
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # set chart title and label axes
 ax.set_title("Squared Numbers", fontsize=24)
@@ -16,3 +17,5 @@ ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
+
+
