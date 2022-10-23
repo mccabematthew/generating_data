2544ad1d4792dc5e22254a9bdf7000ca5a88d445
Matthew McCabe <matthew.james.mccabe@outlook.com>
2022-10-22 23:45:52 -0500

    Created set of x and y values to utilize scatter method

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # plt as alias for pyplot for ease of use
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 # fig represents  'figure' (entire collection of plots), ax reps singular plots
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # set chart title and label axes
 ax.set_title("Squared Numbers", fontsize=24)
@@ -14,8 +15,6 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # set size of tick labels
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
-
-
