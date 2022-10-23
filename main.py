7304a26983544c3fd7bb790e9926cf1ee49c53e9
Matthew McCabe <matthew.james.mccabe@outlook.com>
2022-10-22 23:50:09 -0500

    Changed x and y value lists to create a wider range of ax's and added axis method for minimum and maxim x and y values

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # plt as alias for pyplot for ease of use
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 # fig represents  'figure' (entire collection of plots), ax reps singular plots
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # set chart title and label axes
 ax.set_title("Squared Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-# set size of tick labels
-ax.tick_params(axis='both', which='major', labelsize=14)
+# set rang efor each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
